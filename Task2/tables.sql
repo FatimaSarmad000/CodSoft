92e374c23124e7bbb611a5a2b62d122624970c48
FatimaSarmad000 <145371528+FatimaSarmad000@users.noreply.github.com>
2023-09-18 20:51:36 +0500

    Update tables.sql

--- Task2/tables.sql
+++ Task2/tables.sql
@@ -1,1 +1,27 @@
 
+-- User Registration Table
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+-- Payment Processing Table
+CREATE TABLE payments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  card_number VARCHAR(16) NOT NULL,
+  cvv VARCHAR(3) NOT NULL,
+  cardholder_name VARCHAR(255) NOT NULL,
+  expiry_date DATE NOT NULL
+);
+
+-- Available Flights Table
+CREATE TABLE flights (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  flight_name VARCHAR(255) NOT NULL,
+  airline VARCHAR(255) NOT NULL,
+  departure_airport VARCHAR(255) NOT NULL,
+  destination_airport VARCHAR(255) NOT NULL,
+  amount DECIMAL(10, 2) NOT NULL,
+  departure_time TIME NOT NULL
+);
